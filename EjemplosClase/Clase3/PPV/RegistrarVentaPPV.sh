068a9c5e5a6fd9316c2c34d195f340ffbbec8504
Juan Camilo Rojas <juankdrums14@gmail.com>
2018-05-28 17:14:51 -0300

    Me rindo con el PPV

--- EjemplosClase/Clase3/PPV/RegistrarVentaPPV.sh
+++ EjemplosClase/Clase3/PPV/RegistrarVentaPPV.sh
@@ -1,5 +1,14 @@
 echo "Señal PPV: "
 er="s-\(.*;.*;\)\(.*\);$2;.*-\2-g"
-re='/.*;.*;.*;'$2';.*/'
-sed 
-sed $re$er SignalsAndChanels.dat
+re='.*;.*;.*;'$2';.*'
+grep "$re" SignalsAndChanels.dat > temporal.dat
+sed $er temporal.dat
+c=$(wc -l temporal.dat)
+echo $c
+if [[ '$c' == '1' ]];
+then
+  echo "PPV registrado con éxito"
+else
+  echo "ERROR: Canal no disponible"
+fi
+rm temporal.dat
