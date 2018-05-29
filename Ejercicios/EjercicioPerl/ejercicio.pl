f7ae84d7b279e858c60540228d58d8fa89901fa7
Juan Camilo Rojas <juankdrums14@gmail.com>
2018-05-29 07:30:24 -0300

    Se inicia ejercicio tipo parcial de perl

---
+++ Ejercicios/EjercicioPerl/ejercicio.pl
@@ -0,0 +1,18 @@
+#!/bin/perl
+open(ENTRADA,'<datos/categorias.txt') || die "ERROR: No se puede abrir el archivo de categorías";
+my %servicios = ();
+if (!(-e "datos/categorias.txt"))
+{
+  die "Archivo de categorías inexistente";
+}
+
+while($linea = <ENTRADA>)
+{
+  @valores = split(/;/,$linea);
+  chomp($valores[1]);
+  $servicios{$valores[1]} = $valores[0];
+}
+
+opendir(DIR,'datos');
+@dir = readdir(DIR);
+print "$dir[2]\n";
